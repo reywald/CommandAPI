fd72a60fbf9eee1261e3d8bc360c39cf956a6bae
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-01-02 22:39:34 +0100

    Add CommandContext to solution
    Create Migration and implement Repository to pull data from DB

---
+++ src/CommandAPI/Data/SQLCommandAPIRepo.cs
@@ -0,0 +1,46 @@
+using System.Linq;
+using System.Collections.Generic;
+using CommandAPI.Data;
+using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public class SQLCommandAPIRepo : ICommandAPIRepo
+    {
+
+        private readonly CommandContext _context;
+        public SQLCommandAPIRepo(CommandContext context)
+        {
+            _context = context;
+        }
+        public void CreateCommand(Command command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCommand(Command command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.CommandItems.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
