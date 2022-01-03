22b56919112163f5b0fee7780bdfb6c77bb1fe52
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-01-03 08:42:02 +0100

    Construct Secure ConnectionString
    Generate User Credentials in secrets file and remove from appsettings.

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using Npgsql;
 using CommandAPI.Data;
 
 namespace CommandAPI
@@ -26,9 +27,12 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(
-                Configuration.GetConnectionString("PostgreSqlConnection")
-            ));
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
             services.AddControllers();
             services.AddScoped<ICommandAPIRepo, SQLCommandAPIRepo>();
         }
